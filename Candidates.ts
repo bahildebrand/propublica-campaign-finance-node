056863dca7475c47f02d9303c64b546ebcc99560
Blake Hildebrand <baugusthildebrand@gmail.com>
2018-07-21 11:49:44 -0500

    Add function generator for candidate endpoints

--- Candidates.ts
+++ Candidates.ts
@@ -1,18 +1,28 @@
 import {IEndPoint} from './types';
 import {apiRequest} from './ApiRequest';
 
+export interface Candidates {
+    search(args: any): any;
+    leaders(args: any): any;
+    recent(args: any): any;
+    candidate(args: any): any;
+}
+
 export class Candidates implements IEndPoint {
     baseUrl: string;
 
     constructor(baseUrl: string){
         this.baseUrl = baseUrl + '/' + 'candidates';
     }
+}
 
-    subStrs = {
-        'search': 'search.json?query={candidate}'
-    }
+var subStrs = {
+    'search': 'search.json?query={candidate}',
+    'recent': 'new.json',
+    'leaders': 'leaders/{category}.json',
+    'candidate': '{fec-id}.json'
+}
 
-    search(args: any): any {
-        return apiRequest(this.baseUrl, this.subStrs['search'], args);
-    }
+for (let key in subStrs) {
+    Candidates.prototype[key] =  function(args) { return apiRequest(this.baseUrl, subStrs[key], args); };
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import {CampaignFinance} from './CampaignFinance';
 
 let c = new CampaignFinance('2016');
-c.candidates.search({'candidate': 'wilson'})
+// c.candidates.recent(null)
+// .then((result) => {
+//     console.log(result);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+// c.candidates.leaders({'category': 'candidate-loan'})
+// .then((result) => {
+//     console.log(result);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+c.candidates.candidate({'fec-id': 'P60005915'})
 .then((result) => {
     console.log(result);
 })
