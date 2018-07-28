0075d7ec8e9baf8eac0bcbef70de0d420f64573c
Blake Hildebrand <baugusthildebrand@gmail.com>
2018-07-28 00:19:29 -0500

    Fill out committe class

--- ApiRequest.ts
+++ ApiRequest.ts
@@ -3,7 +3,6 @@ import * as rp from 'request-promise';
 export function apiRequest (baseStr: String, endpoint, args: any): any{
   let urlStr = baseStr + '/' + buildUrlStr(endpoint, args);
 
-  console.log(urlStr);
   return rp({
       headers: {
         'X-API-Key': 'wNS0K9IzjjbbmynKBhBmcSTy6DVt2P092qCkNu53',
--- Committees.ts
+++ Committees.ts
@@ -3,6 +3,10 @@ import {apiRequest} from './ApiRequest';
 
 export interface Committees {
     search(args: any): any;
+    committee(args: any): any;
+    recent(args: any): any;
+    recentSuperpac(args: any): any;
+    filings(args: any): any;
 }
 
 export class Committees implements IEndPoint {
@@ -15,6 +19,11 @@ export class Committees implements IEndPoint {
 
 var subStrs = {
     'search': 'search.json?query={committee}',
+    'committee': '{fec-id}.json',
+    'recent': 'new.json',
+    'recentSuperpac': 'superpacs.json',
+    'filings': '{fec-id}/filings.json',
+    'leadership': 'committees/leadership.json'
 }
 
 for (let key in subStrs) {
